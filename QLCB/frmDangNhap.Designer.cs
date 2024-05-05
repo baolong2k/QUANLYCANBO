8821907775c48b4df60e7f0a7e4aeb4aa0313652
baolo <baolo@DESKTOP-4NM942L>
2024-05-05 13:42:19 +0700

    Add project files.

---
+++ QLCB/frmDangNhap.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace QLCB
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTaiKhoan = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbQuenMatKhau = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Location = new System.Drawing.Point(221, 329);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(130, 30);
+            this.btnDangNhap.TabIndex = 12;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QLCB.Properties.Resources.Huy_hiệu_CAND;
+            this.pictureBox1.Location = new System.Drawing.Point(121, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 155);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(68, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 33);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "PHẦN MỀM QUẢN LÝ CÁN BỘ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtTaiKhoan
+            // 
+            this.txtTaiKhoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtTaiKhoan.Location = new System.Drawing.Point(269, 237);
+            this.txtTaiKhoan.Name = "txtTaiKhoan";
+            this.txtTaiKhoan.Size = new System.Drawing.Size(142, 26);
+            this.txtTaiKhoan.TabIndex = 16;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMatKhau.Location = new System.Drawing.Point(269, 283);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(142, 26);
+            this.txtMatKhau.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(126, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 18);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Tên đăng nhập";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(137, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 18);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Mật Khẩu";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lbQuenMatKhau
+            // 
+            this.lbQuenMatKhau.AutoSize = true;
+            this.lbQuenMatKhau.Location = new System.Drawing.Point(426, 291);
+            this.lbQuenMatKhau.Name = "lbQuenMatKhau";
+            this.lbQuenMatKhau.Size = new System.Drawing.Size(87, 13);
+            this.lbQuenMatKhau.TabIndex = 20;
+            this.lbQuenMatKhau.TabStop = true;
+            this.lbQuenMatKhau.Text = "Quên mật khẩu !";
+            this.lbQuenMatKhau.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(548, 386);
+            this.Controls.Add(this.lbQuenMatKhau);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTaiKhoan);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDangNhap);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.Name = "frmDangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.frmDangNhap_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTaiKhoan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.LinkLabel lbQuenMatKhau;
+    }
+}
---
+++ QLCB/frmDangNhap.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCB
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+            string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\Download\Phân Tích Thiết Kế Hệ Thống\FormLogin.accdb";
+            OleDbConnection connection = new OleDbConnection(connectionString);
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string Username = txtTaiKhoan.Text;
+            string Password = txtMatKhau.Text;
+
+            // Kiểm tra xem username và password có được nhập không
+            if (!string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                // Thực hiện truy vấn để kiểm tra username và password trong cơ sở dữ liệu
+                string sql = "SELECT * FROM Login WHERE Username = @username AND Password = @password";
+                // Thực hiện truy vấn SQL ở đây
+                // Nếu tìm thấy người dùng, tiến hành hiển thị MainForm
+                MainForm mainForm = new MainForm();
+                mainForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng !", "Cảnh báo", MessageBoxButtons.OK);
+            }
+            if (String.IsNullOrEmpty(txtTaiKhoan.Text))
+            {
+                MessageBox.Show("Vui lòng nhập tài khoản!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtTaiKhoan.Focus();
+                return;
+            }
+            if (String.IsNullOrEmpty(txtMatKhau.Text))
+            {
+                MessageBox.Show("Vui lòng nhập mật khẩu!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMatKhau.Focus();
+                return;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmLayLaiMatKhau layLaiMatKhau = new frmLayLaiMatKhau();
+            layLaiMatKhau.Show();
+            this.Hide();
+        }
+
+        private void frmDangNhap_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
